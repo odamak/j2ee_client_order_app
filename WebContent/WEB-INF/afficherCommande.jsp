8d96b8f5bf8ac50a884edd4f0952a04893a01e28
unknown <omardamak1@gmail.com>
2019-07-04 12:13:16 +0100

    Seperate business logic from servlet for add order

--- WebContent/WEB-INF/afficherCommande.jsp
+++ WebContent/WEB-INF/afficherCommande.jsp
@@ -1,33 +1,29 @@
 <%@ page pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <title>Affichage d'une commande</title>
-        <link type="text/css" rel="stylesheet" href="inc/style.css" />
+        <link type="text/css" rel="stylesheet" href="<c:url value="/inc/style.css"/>" />
     </head>
     <body>
-    	<c:import url="/inc/menu.jsp" />
-        <%-- Affichage de la chaîne "message" transmise par la servlet --%>
-        <p class="info">${ message }</p>
-        <c:choose>
-	        <c:when test="${ fields }">
-		        <%-- Puis affichage des données enregistrées dans le bean "commande" transmis par la servlet --%>
-		        <p>Client</p>
-		        <%-- Les 5 expressions suivantes accèdent aux propriétés du client, qui est lui-même une propriété du bean commande --%>
-		        <p>Nom : <c:out value="${ commande.client.nom }" /></p>
-		        <p>Prénom : <c:out value="${ commande.client.prenom }" /></p>
-		        <p>Adresse : <c:out value="${ commande.client.adresse }" /></p>
-		        <p>Numéro de téléphone : <c:out value="${ commande.client.telephone }" /></p>
-		        <p>Email : <c:out value="${ commande.client.email }" /></p>
-		        <p>Commande</p>
-		        <p>Date  : <c:out value="${ commande.date }" /></p> 
-		        <p>Montant  : <c:out value="${ commande.montant }" /></p> 
-		        <p>Mode de paiement  : <c:out value="${ commande.modePaiement }" /></p> 
-		        <p>Statut du paiement  : <c:out value="${ commande.statutPaiement }" /></p> 
-		        <p>Mode de livraison  : <c:out value="${ commande.modeLivraison }" /></p> 
-		        <p>Statut de la livraison  : <c:out value="${ commande.statutLivraison }" /></p>
-		    </c:when>
-	    </c:choose>
+        <c:import url="/inc/menu.jsp" />
+        <div id="corps">
+            <p class="info">${ form.resultat }</p>
+            <p>Client</p>
+            <p>Nom : <c:out value="${ commande.client.nom }"/></p>
+            <p>Prénom : <c:out value="${ commande.client.prenom }"/></p>
+            <p>Adresse : <c:out value="${ commande.client.adresse }"/></p>
+            <p>Numéro de téléphone : <c:out value="${ commande.client.telephone }"/></p>
+            <p>Email : <c:out value="${ commande.client.email }"/></p>
+            <p>Commande</p>
+            <p>Date  : <c:out value="${ commande.date }"/></p> 
+            <p>Montant  : <c:out value="${ commande.montant }"/></p> 
+            <p>Mode de paiement  : <c:out value="${ commande.modePaiement }"/></p> 
+            <p>Statut du paiement  : <c:out value="${ commande.statutPaiement }"/></p> 
+            <p>Mode de livraison  : <c:out value="${ commande.modeLivraison }"/></p> 
+            <p>Statut de la livraison  : <c:out value="${ commande.statutLivraison }"/></p> 
+        </div>
     </body>
 </html>
--- src/com/sdzee/tp/servlets/CreationCommande.java
+++ src/com/sdzee/tp/servlets/CreationCommande.java
@@ -7,95 +7,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
-import com.sdzee.tp.beans.Client;
 import com.sdzee.tp.beans.Commande;
+import com.sdzee.tp.forms.CreationCommandeForm;
 
 public class CreationCommande extends HttpServlet {
-	
-	public final static String SUCCESS_MESSAGE = "Commande créée avec succès !";
-	public static final String ATT_MESSAGE = "message";
-	public static final String ATT_COMMANDE	 = "commande";
-	public static final String ATT_FIELDS = "fields";
-	
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-        /* Affichage de la page d'inscription */
-        this.getServletContext().getRequestDispatcher( "/WEB-INF/creerCommande.jsp" ).forward( request, response );
+    public static final String ATT_COMMANDE = "commande";
+    public static final String ATT_FORM     = "form";
+
+    public static final String VUE_SUCCES   = "/WEB-INF/afficherCommande.jsp";
+    public static final String VUE_FORM     = "/WEB-INF/creerCommande.jsp";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* À la réception d'une requête GET, simple affichage du formulaire */
+        this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
     }
-	
-	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        /*
-         * Récupération des données saisies, envoyées en tant que paramètres de
-         * la requête GET générée à la validation du formulaire
-         */
-        String nom = request.getParameter( "nomClient" );
-        String prenom = request.getParameter( "prenomClient" );
-        String adresse = request.getParameter( "adresseClient" );
-        String telephone = request.getParameter( "telephoneClient" );
-        String email = request.getParameter( "emailClient" );
-        Boolean fields;
 
-        /* Récupération de la date courante */
-        DateTime dt = new DateTime();
-        /* Conversion de la date en String selon le format défini */
-        DateTimeFormatter formatter = DateTimeFormat.forPattern( "dd/MM/yyyy HH:mm:ss" );
-        String date = dt.toString( formatter );
-        double montant;
-        try {
-            /* Récupération du montant */
-            montant = Double.parseDouble( request.getParameter( "montantCommande" ) );
-        } catch ( NumberFormatException e ) {
-            /* Initialisation à -1 si le montant n'est pas un nombre correct */
-            montant = -1;
-        }
-        String modePaiement = request.getParameter( "modePaiementCommande" );
-        String statutPaiement = request.getParameter( "statutPaiementCommande" );
-        String modeLivraison = request.getParameter( "modeLivraisonCommande" );
-        String statutLivraison = request.getParameter( "statutLivraisonCommande" );
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        CreationCommandeForm form = new CreationCommandeForm();
 
-        String message;
-        /*
-         * Initialisation du message à afficher : si un des champs obligatoires
-         * du formulaire n'est pas renseigné, alors on affiche un message
-         * d'erreur, sinon on affiche un message de succès
-         */
-        if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() || montant == -1
-                || modePaiement.isEmpty() || modeLivraison.isEmpty() ) {
-            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\" <c:url value= \"creerCommande.jsp\" /> \">Cliquez ici</a> pour accéder au formulaire de création d'une commande.";
-            fields = false;
-        } else {
-            message = SUCCESS_MESSAGE;
-            fields = true;
-        }
-        /*
-         * Création des beans Client et Commande et initialisation avec les
-         * données récupérées
-         */
-        Client client = new Client();
-        client.setNom( nom );
-        client.setPrenom( prenom );
-        client.setAdresse( adresse );
-        client.setTelephone( telephone );
-        client.setEmail( email );
+        /* Traitement de la requête et récupération du bean en résultant */
+        Commande commande = form.creerCommande( request );
 
-        Commande commande = new Commande();
-        commande.setClient( client );
-        commande.setDate( date );
-        commande.setMontant( montant );
-        commande.setModePaiement( modePaiement );
-        commande.setStatutPaiement( statutPaiement );
-        commande.setModeLivraison( modeLivraison );
-        commande.setStatutLivraison( statutLivraison );
-
-        /* Ajout du bean et du message à l'objet requête */
+        /* Ajout du bean et de l'objet métier à l'objet requête */
         request.setAttribute( ATT_COMMANDE, commande );
-        request.setAttribute( ATT_MESSAGE, message );
-        request.setAttribute( ATT_FIELDS, fields);
+        request.setAttribute( ATT_FORM, form );
 
-        /* Transmission à la page JSP en charge de l'affichage des données */
-        this.getServletContext().getRequestDispatcher( "/WEB-INF/afficherCommande.jsp" ).forward( request, response );
+        if ( form.getErreurs().isEmpty() ) {
+            /* Si aucune erreur, alors affichage de la fiche récapitulative */
+            this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
+        } else {
+            /* Sinon, ré-affichage du formulaire de création avec les erreurs */
+            this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+        }
     }
 }
