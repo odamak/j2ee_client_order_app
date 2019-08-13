eb5ab1e9e9261a8ca07b09334ba6b43cfa202d04
unknown <omardamak1@gmail.com>
2019-08-13 18:16:08 +0100

    Integration MVC

--- WebContent/WEB-INF/upload.jsp
+++ WebContent/WEB-INF/upload.jsp
@@ -5,26 +5,28 @@
     <head>
         <meta charset="utf-8" />
         <title>Envoi de fichier</title>
-        <link type="text/css" rel="stylesheet" href="<c:url value="/inc/style.css"/>">
+        <link type="text/css" rel="stylesheet" href="<c:url value="/inc/style.css"/>" />
     </head>
     <body>
-        <form action="upload" method="post" enctype="multipart/form-data">
+        <form action="<c:url value="/upload" />" method="post" enctype="multipart/form-data">
             <fieldset>
                 <legend>Envoi de fichier</legend>
 
                 <label for="description">Description du fichier</label>
-                <input type="text" id="description" name="description" value="" />
-                <span class="succes"><c:out value="${description}" /></span>
+                <input type="text" id="description" name="description" value="<c:out value="${fichier.description}"/>" />
+                <span class="erreur">${form.erreurs['description']}</span>
                 <br />
                 
 
                 <label for="fichier">Emplacement du fichier <span class="requis">*</span></label>
-                <input type="file" id="fichier" name="fichier" />
-                <span class="succes"><c:out value="${fichier}" /></span>
+                <input type="file" id="fichier" name="fichier" value="<c:out value="${fichier.nom}"/>" />
+                <span class="erreur">${form.erreurs['fichier']}</span>
                 <br />
                 
                 <input type="submit" value="Envoyer" class="sansLabel" />
-                <br />                 
+                <br />
+                
+                <p class="${empty form.erreurs ? 'succes' : 'erreur'}">${form.resultat}</p>        
             </fieldset>
         </form>
     </body>
--- src/com/sdzee/tp/servlets/Upload.java
+++ src/com/sdzee/tp/servlets/Upload.java
@@ -1,132 +1,46 @@
 package com.sdzee.tp.servlets;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+
+import com.sdzee.tp.beans.Fichier;
+import com.sdzee.tp.forms.UploadForm;
 
 public class Upload extends HttpServlet {
-	public static final String VUE = "/WEB-INF/upload.jsp";
-	public static final String CHAMP_DESCRIPTION = "description";
-	public static final String CHAMP_FICHIER     = "fichier";
-	public static final String CHEMIN        = "chemin";
-	public static final int TAILLE_TAMPON = 10240; // 10 ko
+    public static final String CHEMIN      = "chemin";
 
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-		/* Affichage de la page d'envoi de fichiers */
-		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-	}
-	
-	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-	    /*
-	     * Lecture du paramètre 'chemin' passé à la servlet via la déclaration
-	     * dans le web.xml
-	     */
-	    String chemin = this.getServletConfig().getInitParameter( CHEMIN );
+    public static final String ATT_FICHIER = "fichier";
+    public static final String ATT_FORM    = "form";
 
-	    /* Récupération du contenu du champ de description */
-	    String description = request.getParameter( CHAMP_DESCRIPTION );
-	    request.setAttribute( CHAMP_DESCRIPTION, description );
+    public static final String VUE         = "/WEB-INF/upload.jsp";
 
-	    /*
-	     * Les données reçues sont multipart, on doit donc utiliser la méthode
-	     * getPart() pour traiter le champ d'envoi de fichiers.
-	     */
-	    Part part = request.getPart( CHAMP_FICHIER );
-	        
-	    /*
-	     * Il faut déterminer s'il s'agit d'un champ classique 
-	     * ou d'un champ de type fichier : on délègue cette opération 
-	     * à la méthode utilitaire getNomFichier().
-	     */
-	    String nomFichier = getNomFichier( part );
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Affichage de la page d'upload */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
 
-	    /*
-	     * Si la méthode a renvoyé quelque chose, il s'agit donc d'un champ
-	     * de type fichier (input type="file").
-	     */
-	    if ( nomFichier != null && !nomFichier.isEmpty() ) {
-	        String nomChamp = part.getName();
-	        /*
-	         * Antibug pour Internet Explorer, qui transmet pour une raison
-	         * mystique le chemin du fichier local à la machine du client...
-	         * 
-	         * Ex : C:/dossier/sous-dossier/fichier.ext
-	         * 
-	         * On doit donc faire en sorte de ne sélectionner que le nom et
-	         * l'extension du fichier, et de se débarrasser du superflu.
-	         */
-	         nomFichier = nomFichier.substring( nomFichier.lastIndexOf( '/' ) + 1 )
-	                .substring( nomFichier.lastIndexOf( '\\' ) + 1 );
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /*
+         * Lecture du paramètre 'chemin' passé à la servlet via la déclaration
+         * dans le web.xml
+         */
+        String chemin = this.getServletConfig().getInitParameter( CHEMIN );
 
-	        /* Écriture du fichier sur le disque */
-	        ecrireFichier( part, nomFichier, chemin );
+        /* Préparation de l'objet formulaire */
+        UploadForm form = new UploadForm();
 
-	        request.setAttribute( nomChamp, nomFichier );
-	    }
+        /* Traitement de la requête et récupération du bean en résultant */
+        Fichier fichier = form.enregistrerFichier( request, chemin );
 
-	    this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-	}
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute( ATT_FORM, form );
+        request.setAttribute( ATT_FICHIER, fichier );
 
-		
-	/* 
-	 * Méthode utilitaire qui a pour unique but d'analyser l'en-tête "content-disposition",
-	 * et de vérifier si le paramètre "filename"  y est présent. Si oui, alors le champ traité
-	 * est de type File et la méthode retourne son nom, sinon il s'agit d'un champ de formulaire 
-	 * classique et la méthode retourne null. 
-	 */
-	private static String getNomFichier( Part part ) {
-	    /* Boucle sur chacun des paramètres de l'en-tête "content-disposition". */
-	    for ( String contentDisposition : part.getHeader( "content-disposition" ).split( ";" ) ) {
-	    	/* Recherche de l'éventuelle présence du paramètre "filename". */
-	        if ( contentDisposition.trim().startsWith("filename") ) {
-	            /* Si "filename" est présent, alors renvoi de sa valeur, c'est-à-dire du nom de fichier. */
-	        	return contentDisposition.substring( contentDisposition.indexOf( '=' ) + 1 ).trim().replace( "\"", "" );
-	        }
-	    }
-	    /* Et pour terminer, si rien n'a été trouvé... */
-	    return null;
-	}
-	
-	/*
-	 * Méthode utilitaire qui a pour but d'écrire le fichier passé en paramètre
-	 * sur le disque, dans le répertoire donné et avec le nom donné.
-	 */
-	private void ecrireFichier( Part part, String nomFichier, String chemin ) throws IOException {
-	    /* Prépare les flux. */
-	    BufferedInputStream entree = null;
-	    BufferedOutputStream sortie = null;
-	    try {
-	        /* Ouvre les flux. */
-	        entree = new BufferedInputStream( part.getInputStream(), TAILLE_TAMPON );
-	        sortie = new BufferedOutputStream( new FileOutputStream( new File( chemin + nomFichier ) ),
-	                TAILLE_TAMPON );
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
 
-	        /*
-	         * Lit le fichier reçu et écrit son contenu dans un fichier sur le
-	         * disque.
-	         */
-	        byte[] tampon = new byte[TAILLE_TAMPON];
-	        int longueur;
-	        while ( ( longueur = entree.read( tampon ) ) > 0 ) {
-	            sortie.write( tampon, 0, longueur );
-	        }
-	    } finally {
-	        try {
-	            sortie.close();
-	        } catch ( IOException ignore ) {
-	        }
-	        try {
-	            entree.close();
-	        } catch ( IOException ignore ) {
-	        }
-	    }
-	}
 }
